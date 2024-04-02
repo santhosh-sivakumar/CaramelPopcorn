ddd92ca69e72cdb9ec67a2648b4221f20d2e4967
santhosh-sivakumar <santhosh.s2023ai-ds@sece.ac.in>
2024-04-02 15:15:11 +0530

    initial commit

---
+++ Python/0001_Two_Sum/0001_Two_Sum.py
@@ -0,0 +1,4 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        return [[i, j] for i in range(len(nums)) for j in range(i+1, len(nums)) if (nums[i]+nums[j]) == target][0]
