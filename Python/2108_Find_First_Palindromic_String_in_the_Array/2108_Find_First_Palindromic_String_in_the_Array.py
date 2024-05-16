282835ddfd4c173c0df498b16a7bf4b3005842e2
Santhosh. S <2005.santhoshsivakumar@gmail.com>
2024-05-16 13:45:06 +0530

    intial commit

---
+++ Python/2108_Find_First_Palindromic_String_in_the_Array/2108_Find_First_Palindromic_String_in_the_Array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        
+        for i in words:
+            if(i == i[::-1]):
+                return i
+        return ""
